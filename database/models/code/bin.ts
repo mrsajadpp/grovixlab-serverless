6157d3d5a35fb7193e723a20fb314d383280c3be
mrsajadpp <mrsajadpp@gmail.com>
2024-09-21 11:06:48 +0530

    code bina ddded

---
+++ database/models/code/bin.ts
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const codeSchema = new Schema({
+    user_id: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    verification_code: {
+        type: Number,
+        required: true,
+    },
+    created_time: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export const CodeBin = mongoose.model('CodeBin', codeSchema);
