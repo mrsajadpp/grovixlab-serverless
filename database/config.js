e37985e990344ce284b858e10a2e1ba9eeab3d09
mrsajadpp <mrsajadpp@gmail.com>
2024-09-21 10:37:36 +0530

    databse added

---
+++ database/config.js
@@ -0,0 +1,21 @@
+// config.js
+require("dotenv").config();
+const mongoose = require('mongoose'); 
+
+export const connect = async () => {
+    try {
+        mongoose.connection.on('connected', function () {
+            // Hack the database back to the right one, because when using mongodb+srv as protocol.
+            if (mongoose.connection.client.s.url.startsWith('mongodb+srv')) {
+                mongoose.connection.db = mongoose.connection.client.db(process.env.DB_NAME);
+            }
+            console.log('Connection to MongoDB established.')
+        });
+        await mongoose.connect(`${process.env.DB_STRING}`);
+        console.log('🧠 MongoDB connected');
+    } catch (error) {
+        console.error(error);
+        console.error('🔴 Error connecting to MongoDB:', error.message);
+        process.exit(1);
+    }
+};
